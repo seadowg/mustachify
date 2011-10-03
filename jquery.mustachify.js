399ecf767c36d014bae6cc37f2dbe5742b802a53
Chris Brown <xoebus@xoeb.us>
2011-10-03 01:37:13 +0100

    Fixed code to be more in line with README.

--- jquery.mustachify.js
+++ jquery.mustachify.js
@@ -1,6 +1,6 @@
 (function( $ ){
   $.fn.mustachify = function() {
-    $('img').each(function(index) {
+    this.find('img').each(function(index) {
       var src = $(this).attr('src');
       $(this).attr('src', 'http://mustachify.me/?src=' + src)
     })
