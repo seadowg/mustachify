86af9b2ccdae15d16e7eab2aca2028c144c0c21c
Chris Brown <xoebus@xoeb.us>
2011-10-03 01:23:56 +0100

    Refactor.

--- jquery.mustachify.js
+++ jquery.mustachify.js
@@ -1,16 +1,8 @@
 (function( $ ){
   $.fn.mustachify = function() {
-		var stuff = this.find('img');
-		if (stuff.length > 0) {
-			for (i = 0; i < stuff.length; i++) {
-				path = "http://mustachify.me/?src=" + stuff.get(i).src;
-				stuff.get(i).src = path;
-			}
-		}
-		else if (this.get(0).nodeName == "IMG") {
-			path = "http://mustachify.me/?src=" + this.get(0).src;
-			this.get(0).src = path;
-		}
-		return this;
+    $('img').each(function(index) {
+      var src = $(this).attr('src');
+      $(this).attr('src', 'http://mustachify.me/?src=' + src)
+    })
 	}
 })( jQuery );
